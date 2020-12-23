696fc771a32249325639eb332a66fc76efb3fe21
Adebayo Adeleke <>
2020-12-23 20:24:01 +0100

    Initial commit

---
+++ src/main/java/com/exquisappfactory/realtimequestionprocessor/RealtimeQuestionProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.exquisappfactory.realtimequestionprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeQuestionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeQuestionProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exquisappfactory/realtimequestionprocessor/RealtimeQuestionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exquisappfactory.realtimequestionprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeQuestionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
